1b70f5fe359d26eca662746cd59ce0aa0f86a34b
ravi <ronniloreo@gmail.com>
2021-10-18 08:27:32 -0700

    code updated

--- Django-boilerplate/views.py
+++ Django-boilerplate/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'base.html', {})
+    if request.method == 'POST':
+        channel_1 = request.POST.get('channel1')
+        channel_2 = request.POST.get('channel2')
+        channel_3 = request.POST.get('channel3')
+        channel_4 = request.POST.get('channel4')
+        channel_5 = request.POST.get('channel5')
+        channel_6 = request.POST.get('channel6')
+        channel_7 = request.POST.get('channel7')
+        channel_8 = request.POST.get('channel8')
+        
+
+        print(channel_1,channel_2,channel_3,channel_4,channel_5,channel_6,channel_7,channel_8)
+        return HttpResponse('data submitted')
+    return render(request, 'index.html', {})
